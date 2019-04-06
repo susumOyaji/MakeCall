92b6187b1577d0424fac1a62110e8c8a10cddc1e
susumOyaji <sumitomo0210@gmail.com>
2019-04-06 14:51:44 +0900

    mac to up

--- MakeCall/MakeCall.Android/PhoneCall_Droid.cs
+++ MakeCall/MakeCall.Android/PhoneCall_Droid.cs
@@ -26,55 +26,64 @@ namespace MakeCall.Droid
         public void MakeQuickCall(string PhoneNumber)
         {
 
-                var context = Forms.Context;
-               
-                var intent = new Intent(Intent.ActionCall);
-                intent.SetData(Uri.Parse("tel:" + PhoneNumber));
-                context.StartActivity(intent);
-                    
-                
-
-                
+            var context = Forms.Context;
+            /*   
+            var intent = new Intent(Intent.ActionCall);
+            intent.SetData(Uri.Parse("tel:" + PhoneNumber));
+            context.StartActivity(intent);
+            */
 
 
-
-
-
-
-            /*
-            try
-            {
-                var uri = Android.Net.Uri.Parse(string.Format("tel:{0}", PhoneNumber));
-                var intent = new Intent(Intent.ActionCall, uri);
-                
-                Xamarin.Forms.Forms.Context.StartActivity(intent);
-
-            }
-            catch(Exception ex)
-            {
-
-                new AlertDialog.Builder(Android.App.Application.Context).SetPositiveButton("OK", (sender, args) =>
-                 {
-
-                 })
-                .SetMessage(ex.ToString())
-                .SetTitle("Android Exception")
+            var callIntent = new Intent(Intent.ActionCall);
+            new AlertDialog.Builder(context)
+                .SetMessage("Call " + PhoneNumber + "?")
+                .SetNeutralButton("Call", delegate {
+                    callIntent.SetData(Android.Net.Uri.Parse("tel:" + PhoneNumber));
+                    context.StartActivity(callIntent);
+                })
+                .SetNegativeButton("Cancel", delegate { })
                 .Show();
-
-
-                var dlg = new AlertDialog.Builder(Android.App.Application.Context);
-                dlg.SetTitle("タイトル");
-                dlg.SetMessage("メッセージ");
-                dlg.SetPositiveButton( //OKボタンの処理
-                    "OK", (s, a) => Toast.MakeText(Android.App.Application.Context, "OK", ToastLength.Short).Show());
-                dlg.SetNegativeButton( //Cancelボタンの処理
-                    "Cancel", (s, a) => Toast.MakeText(Android.App.Application.Context, "Cancel", ToastLength.Short).Show());
-                dlg.Create().Show();
+        
 
 
 
-            }*/
-            
+
+
+
+        /*
+        try
+        {
+            var uri = Android.Net.Uri.Parse(string.Format("tel:{0}", PhoneNumber));
+            var intent = new Intent(Intent.ActionCall, uri);
+
+            Xamarin.Forms.Forms.Context.StartActivity(intent);
+
         }
+        catch(Exception ex)
+        {
+
+            new AlertDialog.Builder(Android.App.Application.Context).SetPositiveButton("OK", (sender, args) =>
+             {
+
+             })
+            .SetMessage(ex.ToString())
+            .SetTitle("Android Exception")
+            .Show();
+
+
+            var dlg = new AlertDialog.Builder(Android.App.Application.Context);
+            dlg.SetTitle("タイトル");
+            dlg.SetMessage("メッセージ");
+            dlg.SetPositiveButton( //OKボタンの処理
+                "OK", (s, a) => Toast.MakeText(Android.App.Application.Context, "OK", ToastLength.Short).Show());
+            dlg.SetNegativeButton( //Cancelボタンの処理
+                "Cancel", (s, a) => Toast.MakeText(Android.App.Application.Context, "Cancel", ToastLength.Short).Show());
+            dlg.Create().Show();
+
+
+
+        }*/
+
+    }
     }
 }
